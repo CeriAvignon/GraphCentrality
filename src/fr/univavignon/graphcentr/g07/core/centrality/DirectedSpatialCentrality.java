cf087ef8e3d5c74fcfa198156dbc3bce78694022
imane.lounissa <imenelounissa@gmail.com>
2018-05-03 17:46:34 +0200

    ola

---
+++ src/fr/univavignon/graphcentr/g07/core/centrality/DirectedSpatialCentrality.java
@@ -0,0 +1,15 @@
+package fr.univavignon.graphcentr.g07.core.centrality;
+
+import fr.univavignon.graphcentr.g07.core.AbstractCentrality;
+import fr.univavignon.graphcentr.g07.core.graphs.DirectedSpatialGraph;
+
+/**
+ * 
+ * @author Holstein Kelian
+ * 
+ * @brief Used to compute centrality on spatial & directed graph
+ */
+public interface DirectedSpatialCentrality extends AbstractCentrality<DirectedSpatialGraph>
+{
+	// Nothing to do
+}
