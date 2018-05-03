b80222b4edc092d2514feaddfb91d2c7de69db45
mustapha.djebablia <dm_djebablia@esi.dz>
2018-05-03 16:37:23 +0200

    TEST

---
+++ src/fr/univavignon/graphcentr/g11/DirectedWeightedEccentricity.java
@@ -0,0 +1,15 @@
+package fr.univavignon.graphcentr.g11;
+
+import fr.univavignon.graphcentr.g07.core.centrality.CentralityResult;
+import fr.univavignon.graphcentr.g07.core.centrality.DirectedWeightedCentrality;
+import fr.univavignon.graphcentr.g07.core.graphs.DirectedWeightedGraph;
+
+public class DirectedWeightedEccentricity implements DirectedWeightedCentrality {
+
+	@Override
+	public CentralityResult evaluate(DirectedWeightedGraph inGraph) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/fr/univavignon/graphcentr/g11/WeightedEccentricity.java
@@ -0,0 +1,15 @@
+package fr.univavignon.graphcentr.g11;
+
+import fr.univavignon.graphcentr.g07.core.centrality.CentralityResult;
+import fr.univavignon.graphcentr.g07.core.centrality.WeightedCentrality;
+import fr.univavignon.graphcentr.g07.core.graphs.WeightedGraph;
+
+public class WeightedEccentricity implements WeightedCentrality{
+
+	@Override
+	public CentralityResult evaluate(WeightedGraph inGraph) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
