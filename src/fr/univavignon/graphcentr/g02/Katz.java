b0e86e2dd128e5f2913efd3958a7763d8fc54255
Loic MENGUY <loicm97@gmail.com>
2018-05-05 20:18:57 +0200

    Implemented Katz Algorithm

--- src/fr/univavignon/graphcentr/g02/Katz.java
+++ src/fr/univavignon/graphcentr/g02/Katz.java
@@ -1,10 +1,12 @@
 package fr.univavignon.graphcentr.g02;
 
-import java.util.*;
-
-import fr.univavignon.graphcentr.g07.core.graphs.DirectedGraph;
-import fr.univavignon.graphcentr.g07.core.centrality.DirectedCentrality;
 import fr.univavignon.graphcentr.g07.core.centrality.CentralityResult;
+import fr.univavignon.graphcentr.g07.core.centrality.DirectedCentrality;
+import fr.univavignon.graphcentr.g07.core.graphs.DirectedGraph;
+
+import cern.colt.matrix.impl.*;
+import cern.colt.matrix.linalg.*;
+import cern.jet.math.*;
 
 public class Katz implements DirectedCentrality
 {
@@ -17,11 +19,58 @@ public class Katz implements DirectedCentrality
 		return result;
 	}
 	
-	private Vector KatzAlgorithm(DirectedGraph inGraph)
+	private double[] KatzAlgorithm(DirectedGraph inGraph, float alpha)
 	{
-		int n = inGraph.getNodeCount();
-		Vector result = new Vector(n);
+		int n = inGraph.getNodeCount(); 														//Number of nodes in the graph
+		double[][] adjacencyMatrix = inGraph.toAdjacencyMatrix(); 								//Converting graph to adjacency matrix (to be able to use Colt functions)
 		
+		//Creating identity matrix
+		double[][] identityMatrix = new double[n][n]; 
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j < n; j++)
+			{
+				if(i==j)
+				{
+					identityMatrix[i][j] = 1;
+				}
+			}
+		}
+		
+		double[] result = new double[n]; 														//Return vector
+		for(int i = 0; i < n; i++)																//Initializing all elements of return vector at 1
+		{
+			result[i] = 1;
+		}
+		DenseDoubleMatrix1D coltResult = new DenseDoubleMatrix1D(result); 						//Return vector using Colt needed type
+		
+		DenseDoubleMatrix2D coltInGraph = new DenseDoubleMatrix2D(adjacencyMatrix); 			//Creating a matrix using the Colt needed type
+		DenseDoubleMatrix2D coltIdentityMatrix = new DenseDoubleMatrix2D(identityMatrix);		//Indentity matric using Colt needed type
+		DenseDoubleMatrix2D coltTransposedMatrix = (DenseDoubleMatrix2D)coltInGraph.viewDice(); //Transposing Matrix
+		
+		coltTransposedMatrix.assign(Functions.mult(alpha));										//Multiplying all values in matrix by alpha.
+		coltTransposedMatrix = (DenseDoubleMatrix2D)coltIdentityMatrix.assign(coltTransposedMatrix, Functions.minus); // I - alpha*transposedMatrix
+		
+		Algebra varAlgebra = new Algebra(); //To be able to use inverse function
+		coltTransposedMatrix = (DenseDoubleMatrix2D)varAlgebra.inverse(coltTransposedMatrix);	//Inversing matrix
+		
+		coltTransposedMatrix = (DenseDoubleMatrix2D)coltIdentityMatrix.assign(coltIdentityMatrix, Functions.minus); // ((I - alpha*transposedMatrix)^-1) - I
+		
+		coltResult = (DenseDoubleMatrix1D)coltTransposedMatrix.zMult(coltResult, coltResult);	//Multiplying Matrix by vector fulfilled with 1
+
+		
+		//Normalization
+		// s computing
+		double s = Arithmetic.factorial(n);
+		
+		s = s * Math.pow(alpha, n-1) * Math.exp(1/alpha);
+		
+		coltResult.toArray(result); //Converting colt format vector to array
+		
+		for(int i = 0; i < n; i++)	//Normalizing all values											
+		{
+			result[i] = result[i] / s;
+		}
 		
 		return result;
 		
