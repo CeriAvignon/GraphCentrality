870252143fd9189bf4f4ee1d69a5e6026967b735
Ahmet Agbektas <zamanyildiz@gmail.com>
2018-05-10 01:34:11 +0200

    Update algorithm

--- src/fr/univavignon/graphcentr/g04/ClosenessDirected.java
+++ src/fr/univavignon/graphcentr/g04/ClosenessDirected.java
@@ -1,6 +1,5 @@
 package fr.univavignon.graphcentr.g04;
 
-import fr.univavignon.graphcentr.g07.core.Node;
 import fr.univavignon.graphcentr.g07.core.centrality.CentralityResult;
 import fr.univavignon.graphcentr.g07.core.centrality.DirectedCentrality;
 import fr.univavignon.graphcentr.g07.core.graphs.DirectedGraph;
@@ -9,7 +8,6 @@ import fr.univavignon.graphcentr.g11.FloydWarshall;
 
 /**
  * @author Agbektas Ahmet
- *
  */
 
 public class ClosenessDirected implements DirectedCentrality
@@ -21,35 +19,56 @@ public class ClosenessDirected implements DirectedCentrality
 		orientation = o;
 	}
 	
-	@Override
-	public CentralityResult evaluate(DirectedGraph inGraph)
+	public String getOrientation()
 	{
-		CentralityResult result = new CentralityResult();
-
-		double somme = 0;
-		int n = inGraph.getNodeCount();
-
-		double distance[][]= FloydWarshall.findShortestDistances(inGraph);	
-		
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				if (i != j)
-				{
-					if (orientation == "in")
-					{
-						somme += distance[j][i];
-					}
-					else
-					{
-						somme += distance[i][j];
-					}
-				}
-			}
-			result.add((n-1)/somme);
-		}
-
-		return result;
+		return orientation;
 	}
+	
+	@Override
+    public CentralityResult evaluate(DirectedGraph inGraph)
+    {
+        CentralityResult result = new CentralityResult();
+
+        double somme = 0;
+        int n = inGraph.getNodeCount();
+
+        double distance[][] = FloydWarshall.findShortestDistances(inGraph);
+        
+        for (int i = 0; i < n; i++)
+        {
+            somme = 0;
+            
+            for (int j = 0; j < n; j++)
+            {
+                if (i != j)
+                {
+                    if (getOrientation() == "in")
+                    {
+                        if (distance[j][i] != Double.MAX_VALUE)
+                        {
+                            somme += distance[j][i];
+                        }
+                    }
+                    else
+                    {
+                        if (distance[i][j] != Double.MAX_VALUE)
+                        {
+                            somme += distance[i][j];
+                        }
+                    }
+                }
+            }
+            
+            if (somme == 0)
+            {
+                result.add(null);
+            }
+            else
+            {
+                result.add((n-1)/somme);
+            }
+        }
+
+        return result;
+    }
 }
