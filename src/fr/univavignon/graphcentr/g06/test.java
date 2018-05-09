7ba5493a1b0a270c8e696b08d322d1e3e857e1df
cc222ip <32032587+cc222ip@users.noreply.github.com>
2018-05-09 13:53:22 +0200

    g07 core

--- src/fr/univavignon/graphcentr/g06/test.java
+++ src/fr/univavignon/graphcentr/g06/test.java
@@ -1,6 +1,7 @@
 package fr.univavignon.graphcentr.g06;
 
-import fr.univavignon.graphcentr.g07.Node;
+import fr.univavignon.graphcentr.g07.core.Link;
+import fr.univavignon.graphcentr.g07.core.Node;
 
 public class test {
 
