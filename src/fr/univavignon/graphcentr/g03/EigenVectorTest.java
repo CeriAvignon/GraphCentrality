59c1e924751a7cf55e49e19eef25a7d5298a44b4
Delfeil Casanova <delfeilcasanova@gmail.com>
2018-05-10 13:48:07 +0200

    Comit3: Ajout eigenCentrality

---
+++ src/fr/univavignon/graphcentr/g03/EigenVectorTest.java
@@ -0,0 +1,15 @@
+package fr.univavignon.graphcentr.g03;
+
+import fr.univavignon.graphcentr.g07.core.graphs.SimpleGraph;
+import fr.univavignon.graphcentr.g07.core.centrality.CentralityResult;
+
+
+public class EigenVectorTest
+{
+	public static void main(String[] args)
+	{
+		SimpleGraph g=new SimpleGraph();
+		
+		g.createNode();
+	}
+}
