4d8240a386ffd605884b27eae84c4cb87e6dc6b7
Hamza Abidi <abidi.hamza84000@gmail.com>
2018-05-10 18:18:28 +0200

    SimpleDegreeCentrality

---
+++ src/fr/univavignon/graphcentr/g04/DegreeCentrality.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package fr.univavignon.graphcentr.g04;
+
+import fr.univavignon.graphcentr.g07.core.centrality.CentralityResult;
+import fr.univavignon.graphcentr.g07.core.centrality.SimpleCentrality;
+import fr.univavignon.graphcentr.g07.core.graphs.SimpleGraph;
+import fr.univavignon.graphcentr.g07.core.AbstractSimpleGraph.java
+
+/**
+ * @author Abidi Hamza
+ *
+ */
+public class SimpleDegreeCentrality implements SimpleCentrality{
+
+	@Override
+    public CentralityResult evaluate(SimpleGraph inGraph)
+    {
+        CentralityResult result = new CentralityResult();
+
+        int n = inGraph.getNodeCount();
+        
+        for (int i = 0; i < n; i++)
+        {
+        	result.add(inGraph.getNodeAt(i) / n - 1);
+        }
+        
+        return result;
+    }
+}
