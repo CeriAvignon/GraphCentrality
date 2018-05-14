aee2e92a1319b9d0ae873b52c0ff76322bb36642
Thomas Duvignau <thomaslaforce@yahoo.fr>
2018-05-14 17:38:00 +0200

    CalculProba add

--- src/fr/univavignon/graphcentr/g13/EntropyCentrality.java
+++ src/fr/univavignon/graphcentr/g13/EntropyCentrality.java
@@ -11,10 +11,10 @@ import fr.univavignon.graphcentr.g07.core.graphs.SimpleGraph;
 
 public class EntropyCentrality implements SimpleCentrality
 {
-	
-	
+
+
 	public void ElemPath(SimpleGraph inGraph, int v1, int v2,  ArrayList Parcours, ArrayList[] Res, int v3)
-	 {
+	{
 		 Parcours.add(v3);
 		 if(v2 == v3)
 		 {
@@ -52,9 +52,80 @@ public class EntropyCentrality implements SimpleCentrality
 			 }
 		 }
 	 }
+	
+	public int tau(int a)
+	{
+		return 1;
+	}
+	
+	public int tau2(int a)
+	{
+		return 1;
+	}
+	
+	public int calculProba(SimpleGraph inGraph,int i,int j)
+	{
+		int z = 0;
+		int b;
+		
+		if(i != j)
+		{
+				ArrayList res = new ArrayList();
+				ArrayList parcours = new ArrayList();
+				
+				ElemPath(inGraph,i,j,parcours,res);
+					
+				int k=0;
+				for( k=0 ; k<res.size(); k++)
+				{
+					int y=1;
+					int t=1;
+					for(t=1;t<res.get(1).size()-1;t++)
+					{
+						b=tau(res.get(k).get(t));
+						y=y*b;
+					}
+					y=y*tau2(j);
+					z=z+y;
+				}
+		}
+		else
+		{
+			Node v=inGraph.getNodeAt(i);
+			z= 1 / (inGraph.getNodeDegree(v));
+		}
+		
+		return z;	
+	}
+
+	public void AlgoPrincipale(SimpleGraph inGraph,boolean normalise)
+	{
+		int n = inGraph.getNodeCount();
+
+		double[] C_Ent = new double[n];
+		
+		double x=0;
+
+		for(int i = 0; i <= n ; i++)
+		{
+			for(int j = 0; j <=n ; j++)
+			{
+				x = x + calculProba(inGraph,i,j) * Math.log( calculProba(inGraph,i,j) );
+			}
+			if(normalise == true)
+			{
+				x = x * (-1) / Math.log(n);
+			}
+
+			C_Ent[i] = x;	  			
+		}
+	}
 
 	@Override
 	public CentralityResult evaluate(SimpleGraph inGraph) {
+		
+
+		
 		// TODO Auto-generated method stub
 		return null;
 	}
