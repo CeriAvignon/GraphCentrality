f30892f217e9c2eafee915551eccea4187e9157f
Colin Riviere <riviere.colin1s3@gmail.com>
2018-05-19 12:45:36 -0700

    Add Test

--- src/fr/univavignon/graphcentr/tests/g01/CliqueTest.java
+++ src/fr/univavignon/graphcentr/tests/g01/CliqueTest.java
@@ -1,17 +1,20 @@
 package fr.univavignon.graphcentr.tests.g01;
 
-import fr.univavignon.graphcentr.g01.Laverage;
+
+import fr.univavignon.graphcentr.g01.Clique;
 import fr.univavignon.graphcentr.g07.core.centrality.CentralityResult;
 import fr.univavignon.graphcentr.g07.core.graphs.SimpleGraph;
+
 
 /**
  * @author Riviere Colin, Benoit Loris
  *
  */
-public class LaverageTest {
+public class CliqueTest {
+
 	
 	/**
-	 * Execute Leverage Test
+	 * Execute Clique Test
 	 */
 	public static void execute()
 	{
@@ -23,19 +26,25 @@ public class LaverageTest {
 		graph.createNode();
 		graph.createNode();
 		graph.createNode();
+		graph.createNode();
 		
 		graph.createLink(0, 1);
+		graph.createLink(0, 2);
+		graph.createLink(0, 3);
 		graph.createLink(1, 2);
 		graph.createLink(1, 3);
 		graph.createLink(2, 3);
-		graph.createLink(3, 6);
 		graph.createLink(3, 4);
-		graph.createLink(4, 5);
-		graph.createLink(5, 6);
+		graph.createLink(3, 5);
+		graph.createLink(3, 6);
+		graph.createLink(3, 7);
+		graph.createLink(6, 7);
 		
-		Laverage centrality = new Laverage();	
+		
+		Clique centrality = new Clique();
 		CentralityResult result = centrality.evaluate(graph);
 		System.out.println(result);
+		
+		
 	}
-
 }
