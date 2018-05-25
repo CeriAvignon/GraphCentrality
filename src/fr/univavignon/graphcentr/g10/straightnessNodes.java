f730b56ee50dc5530b0201fb441493313d9fe51a
mohamedhabib06 <mohamed-habib06@outlook.fr>
2018-05-25 02:25:48 +0200

    implimentation de Straightness

--- src/fr/univavignon/graphcentr/g10/straightnessNodes.java
+++ src/fr/univavignon/graphcentr/g10/straightnessNodes.java
@@ -6,6 +6,7 @@ import fr.univavignon.graphcentr.g07.core.WeightedLink;
 
 import fr.univavignon.graphcentr.g07.core.helper.WeightedGraphHelper;
 import fr.univavignon.graphcentr.g07.core.helper.SpatialGraphHelper;
+import fr.univavignon.graphcentr.g11.FloydWarshall;
 /**
  * @author Habib Mohamed
  * 
@@ -46,4 +47,32 @@ public static double StraightnessNodes(SpatialNode k,SpatialNode j,double distan
 	{
 	return Math.sqrt(Math.pow((u.getX()-v.getX()),2)+Math.pow((u.getY()-v.getY()),2));
 	}
+
+public static double[] fin(AbstractGraph inGraph)
+{
+
+
+int n = inGraph.getNodeCount();
+double[][] adjacencyMatrix = inGraph.toAdjacencyMatrix();
+double[][] distanceShortestPathMatrix = findShortestDistances(inGraph);
+
+double moyenne[] = new double[n];
+
+for(int k=0;k<n;k++)
+{
+	for(int j=0;j<n;j++)
+	{
+		if(k!=j)
+		{
+			moyenne[k]=moyenne[k]+ Straightness(inGraph.getNodeAt(k),inGraph.getNodeAt(j),distanceShortestPathMatrix[k][j]);
+		}
+	}
+}
+moyenne k ← moyenne k /(n − 1);
+
+return moyenne ;
+
+}
+
+
 }
