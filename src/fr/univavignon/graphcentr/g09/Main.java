88cf5bdf07677349a7d794f46297ea4822868a05
Mehdi <mehdiayache@hotmail.fr>
2018-05-25 19:49:06 +0200

    commit

---
+++ src/fr/univavignon/graphcentr/g09/Main.java
@@ -0,0 +1,12 @@
+import cern.colt.matrix.impl.DenseDoubleMatrix2D;
+
+public class Main {
+	public static void main(String r[]) {
+		DoubleMatrix2D inputG; // de l exemple
+		DoubleMatrix2D inputI;
+		float b =  inputB;
+		PowerCentrality pw = new PowerCentrality(inputG,inputI);
+		System.out.println(pw._PowerCentrality(b));
+		// Il n y a que l exemple 1.
+	}
+}
