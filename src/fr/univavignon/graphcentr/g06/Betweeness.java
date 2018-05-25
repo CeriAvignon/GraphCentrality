32e91dc06ef1fc53e11a2cab183116eb6f72c59b
Noaman Tata <noaman.t@hotmail.fr>
2018-05-25 12:37:49 +0200

    Add 3rd for loop + maj of dist and sigma

--- src/fr/univavignon/graphcentr/g06/Betweeness.java
+++ src/fr/univavignon/graphcentr/g06/Betweeness.java
@@ -12,7 +12,7 @@ import fr.univavignon.graphcentr.g07.core.graphs.DirectedGraph;
 
 
 /**
- * @author Chris_Chevalier
+ * @author Chris_Chevalier, Tata Noaman
  *
  */
 public class Betweeness implements DirectedCentrality {
@@ -20,14 +20,15 @@ public class Betweeness implements DirectedCentrality {
 	@Override
 	public CentralityResult evaluate(DirectedGraph inGraph) {
 
-		Stack<Node> P = new Stack<Node>();
-		Queue<Node> F = new LinkedList<Node>();
+		Stack<Node> P = new Stack<Node>(); 			// Pile permettant de conserver les arrêtes dans un ordre non croissant des distance depuis s
+		Queue<Node> F = new LinkedList<Node>();		// File 
 		
 		int dist[] = new int[inGraph.getNodeCount()];
 		List<Node> pred = new LinkedList<Node>(); // Liste des prédecesseurs
 		
-		List<Integer> sigma = new LinkedList<Integer>();// Nombre de chemins le plus court
-		List<Integer> delta = new LinkedList<Integer>();
+		//List<Integer> sigma = new LinkedList<Integer>();// Nombre de chemins le plus court
+		int sigma[] = new int[inGraph.getNodeCount()];
+		List<Integer> delta = new LinkedList<Integer>();// dépendance de s par rapport à v
 		
 		// Boucle principale :
 		for (Node s : inGraph.getNodes()) {
@@ -35,12 +36,16 @@ public class Betweeness implements DirectedCentrality {
 				pred.clear();
 			}
 			
-			/*for (int t : inGraph) {
+			for (int t=0 ; t < inGraph.getNodeCount(); t++) {
 				dist[t] = -1;
-			}*/
+				sigma[t] = 0;
+			}
+			dist[inGraph.getNodeIndex(s)] = 0; // inGraph.getNode(s) plus ou moin la coord du noeud S
+			sigma[inGraph.getNodeIndex(s)] = 1;
 		}
 		
 		return null;
 	}
 	
 }
+	
